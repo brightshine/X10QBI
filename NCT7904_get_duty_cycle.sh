27fa79f5d7424cba0198f888f2f7ad48a8add895
Brightshine <brightshine.hsieh@gmail.com>
2015-09-17 13:35:23 +0800

    Support get/set duty cycle for multiple PWM controllers

--- NCT7904_get_duty_cycle.sh
+++ NCT7904_get_duty_cycle.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 IP=${1}
-[ -z ${2} ] && USER='ADMIN' || USER=${2}
-[ -z ${3} ] && PASSWD='ADMIN' || PASSWD=${3}
+controller=${2}
+[ -z ${3} ] && USER='ADMIN' || USER=${3}
+[ -z ${4} ] && PASSWD='ADMIN' || PASSWD=${4}
 
 CMD_PREFIX="ipmitool -H ${IP} -U ${USER} -P ${PASSWD} raw "
 
-${CMD_PREFIX} 0x30 0x90 0x5c 0x03 0x10 0x01
+${CMD_PREFIX} 0x30 0x90 0x5c 0x03 0x1${controller} 0x01
--- NCT7904_set_duty_cycle.sh
+++ NCT7904_set_duty_cycle.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 IP=${1}
-duty_cycle=${2}
-[ -z ${3} ] && USER='ADMIN' || USER=${3}
-[ -z ${4} ] && PASSWD='ADMIN' || PASSWD=${4}
+controller=${2}
+duty_cycle=${3}
+[ -z ${4} ] && USER='ADMIN' || USER=${4}
+[ -z ${5} ] && PASSWD='ADMIN' || PASSWD=${5}
 
 CMD_PREFIX="ipmitool -H ${IP} -U ${USER} -P ${PASSWD} raw "
 
-${CMD_PREFIX} 0x30 0x91 0x5c 0x03 0x10 0x${duty_cycle}
+${CMD_PREFIX} 0x30 0x91 0x5c 0x03 0x1${controller} 0x${duty_cycle}
